349638416374ae79cec8c1aadb49c865406b5d04
Mahammadshameer <shaiky900@gmail.com>
2024-11-21 17:38:00 +0530

    Done With the datatypes

---
+++ DataTypes.js
@@ -0,0 +1,8 @@
+let customerID=9849343
+let customerName="Rajesh"
+let activeCard= true
+let LoadActive=null
+let customerAddress=undefined
+console.table([customerID,customerName,activeCard,LoadActive,customerAddress])
+console.log(typeof customerAddress)
+console.log(typeof customerAddress)
--- Variables.js
+++ Variables.js
@@ -3,4 +3,5 @@ let CustomerName="Shameer"
 let CustomerEmail="XXXXXXXXXXXXXXXXX"
 CustomerNumber=90898979
 var CustomerAddress="hyderabad"
+/* Most of the times "var" is not a good practice , because of block-Scope and functional issues */
 console.table([CustomerEmail,CustomerId,CustomerName,CustomerNumber,CustomerAddress])
